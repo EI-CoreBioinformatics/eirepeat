be623b413e4cbbb854e0920d3b760bf294557440
Gemy Kaithakottil <gemygk@gmail.com>
2023-03-01 18:43:28 +0000

    Bump version: 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [line.rstrip() for line in open("requirements.txt", "rt")]
 
 setup(
     name="eirepeat",
-    version="1.2.0",
+    version="1.3.0",
     description="eirepeat - EI Repeat Identification pipeline",
     author="Gemy Kaithakottil",
     author_email="Gemy.Kaithakottil@earlham.ac.uk",
